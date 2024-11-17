82639c3010540bf860120239311bf21c5770b9e5
Istahak <sistahak900@gmail.com>
2024-11-17 15:04:18 +0600

    added file structue and spring boot init

---
+++ Backend/seedhope/src/main/java/com/example/seedhope/seedhope/SeedhopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.seedhope.seedhope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedhopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedhopeApplication.class, args);
+	}
+
+}
