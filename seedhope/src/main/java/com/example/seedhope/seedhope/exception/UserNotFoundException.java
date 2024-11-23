a8a7279c470f0013285729de205bbf9a6cf7f4b3
Abdullah Al Tanzim <77851803+Tanzim34@users.noreply.github.com>
2024-11-23 19:49:44 +0600

    Delete seedhope directory

--- seedhope/src/main/java/com/example/seedhope/seedhope/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.seedhope.seedhope.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
