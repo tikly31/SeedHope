e82eb225e8e31f238a37dac66812b38ef18cb1d2
Abdullah Al Tanzim <tanzimshafin411@gmail.com>
2024-11-24 00:10:21 +0600

    Revert "Delete seedhope directory"
    This reverts commit a8a7279c470f0013285729de205bbf9a6cf7f4b3.

---
+++ seedhope/src/main/java/com/example/seedhope/seedhope/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.seedhope.seedhope.controller;
+
+import com.example.seedhope.seedhope.model.User;
+import com.example.seedhope.seedhope.repository.UserRepository;
+import com.example.seedhope.seedhope.service.Userservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private Userservice userservice;
+    @RequestMapping("/users")
+    public List<User> getAllUsers() {
+        return userservice.getAllUsers();
+    }
+
+    @PostMapping("/users")
+    public User addUser(@RequestBody User user) {
+        return userservice.addUser(user);
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userservice.register(user);
+
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        return userservice.verify(user);
+    }
+
+}
