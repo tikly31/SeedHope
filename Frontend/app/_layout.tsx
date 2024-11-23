276bca9928a161a18cf895781e3ea4f1e6395648
Abdullah Al Tanzim <tanzimshafin411@gmail.com>
2024-11-24 00:10:12 +0600

    Revert "frontned login"
    This reverts commit 8bae6c527c337572c2f0eac15092b336f7f81405.

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -1,31 +1,39 @@
-import { StyleSheet } from "react-native";
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import HomeScreen from "./src/screen/HomeScreen";
-import LoginScreen from "./src/screen/LogInScreen";
-import SignupScreen from "./src/screen/SignUpScreen";
-const Stack = createNativeStackNavigator();
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
 
-const _layout = () => {
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    // <GestureHandlerRootView style={{ flex: 1 }}>
-      // <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name={"HOME"} component={HomeScreen} />
-          <Stack.Screen name={"LOGIN"} component={LoginScreen} />
-          <Stack.Screen name={"SIGNUP"} component={SignupScreen} />
-        </Stack.Navigator>
-      // </NavigationContainer>
-    // </GestureHandlerRootView>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
-};
-
-export default _layout;
-
-const styles = StyleSheet.create({});
+}
--- Frontend/app/src/utils/colors.js
+++
@@ -1,8 +0,0 @@
-export const colors = {
-    white: '#FFFFFF',
-    primary: '#45484A',
-    secondary: '#AEB5BB',
-    gray: '#D9D9D9',
-  };
-
-export default colors;
