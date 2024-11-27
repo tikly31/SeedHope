9eb2c7e934c993ffdfaf2e65776565609348a445
Istahak <sistahak900@gmail.com>
2024-11-27 15:25:52 +0600

    tring to add backend

--- Backend/seedhope/src/main/java/com/example/seedhope/seedhope/model/User.java
+++ Backend/seedhope/src/main/java/com/example/seedhope/seedhope/model/User.java
@@ -15,6 +15,7 @@ public class User {
     private String username;
     private String password;
 
+    private String phone;
     // getters and setters
     public Long getId() {
         return id;
@@ -45,6 +46,12 @@ public class User {
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     @Override
